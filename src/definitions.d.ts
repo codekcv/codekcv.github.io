fd418989dea9134d2489715ae61fb49d8c230d91
Christian Villamin <ChristianVillamin31@gmail.com>
2019-11-10 20:35:05 +0800

    cleared definitions

--- src/definitions.d.ts
+++ src/definitions.d.ts
@@ -1,18 +1,3 @@
-declare module '*.svg' {
-  const content: any;
-  export default content;
-}
-
-declare module '*.jpg' {
-  const content: any;
-  export default content;
-}
-
-declare module '*.png' {
-  const content: any;
-  export default content;
-}
-
 declare module 'disable-scroll' {
   const content: any;
   export default content;
