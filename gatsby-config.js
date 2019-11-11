68bdfaa6f4121a9acdf8dba649a491ad63bf3ec0
Christian Villamin <ChristianVillamin31@gmail.com>
2019-11-11 16:17:31 +0800

    rmv unnecessary dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,6 @@ module.exports = {
     siteUrl: 'https://ChristianVillamin.github.io',
   },
   plugins: [
-    `gatsby-plugin-playground`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
@@ -42,13 +41,8 @@ module.exports = {
         start_url: '/',
         background_color: '#6b37bf',
         theme_color: '#6b37bf',
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: 'standalone',
-        icon: 'src/images/sansfav.png', // This path is relative to the root of the site.
-        // An optional attribute which provides support for CORS check.
-        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-        // Any invalid keyword or empty string defaults to `anonymous`
+        icon: 'src/images/sansfav.png',
         crossOrigin: `use-credentials`,
       },
     },
